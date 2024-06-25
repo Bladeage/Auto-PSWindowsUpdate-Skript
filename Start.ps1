032fe4db3087cbb40d22608dd1212069ae73f265
Bladeage <fageyer@posteo.de>
2024-06-25 19:12:55 +0200

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,13 +1,45 @@
 # PowerShell Script to start and download the automated install of the PowerShell Module "PSWindowsUpdate"
 
-# Install NuGet and Install PSWindowsUpdate Module
+# Allow execution of unsigned PS code
 Set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned'
+
+
+# Install NuGet and Install PSWindowsUpdate Module
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 
-# Download the continous script to continue the process after reboot
-Invoke-WebRequest "https://raw.githubusercontent.com/Bladeage/Auto-PSWindowsUpdate-Skript/main/Start.ps1" -OutFile "$env:LOCALAPPDATA\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+# [Broken] Download the continous script to continue the process after reboot
+#Invoke-WebRequest "https://raw.githubusercontent.com/Bladeage/Auto-PSWindowsUpdate-Skript/main/Start.ps1" -OutFile "$env:LOCALAPPDATA\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+# Prüfen, ob das Modul PSWindowsUpdate installiert ist
+if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+    Write-Output "Das Modul PSWindowsUpdate ist nicht installiert. Installation wird gestartet..."
+
+    # NuGet Provider installieren, falls notwendig
+    if (-not (Get-PackageProvider -ListAvailable -Name NuGet)) {
+        Write-Output "NuGet Provider wird installiert..."
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    }
+
+    # Modul PSWindowsUpdate installieren
+    Write-Output "PSWindowsUpdate Modul wird installiert..."
+    Install-Module -Name PSWindowsUpdate -Force
+
+    # Modul importieren
+    Write-Output "PSWindowsUpdate Modul wird importiert..."
+    Import-Module PSWindowsUpdate
+} else {
+    Write-Output "Das Modul PSWindowsUpdate ist bereits installiert."
+}
+
+# Prüfen, ob das Modul erfolgreich importiert wurde
+if (Get-Module -Name PSWindowsUpdate) {
+    Write-Output "Das Modul PSWindowsUpdate wurde erfolgreich importiert."
+} else {
+    Write-Output "Fehler beim Importieren des Moduls PSWindowsUpdate."
+}
+
 
 # Start the Windows Update process via the PowerShell module
 Get-WindowsUpdate -Install -AcceptAll -AutoReboot
